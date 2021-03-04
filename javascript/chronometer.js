6332f2c7ebd004b8898e7adec207c03569b32fcf
koji <koji4386@gmail.com>
2021-03-04 17:49:00 +0100

    lab chronometer part 1 done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,49 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
     // ... your code goes here
   }
-  startClick(callback) {
+  startClick(callback) {  
+  const intervalId = setInterval(() =>{
+
+  this.currentTime++; 
+
+}, 1000);
     // ... your code goes here
   }
   getMinutes() {
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
     // ... your code goes here
   }
   getSeconds() {
+    let seconds = Math.floor(this.currentTime % 60);
+    return seconds;
     // ... your code goes here
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
+    if (number < 10){
+      return `0${number}`;
+    } else {
+      return `${number}`;
+    } 
     // ... your code goes here
   }
+
   stopClick() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
   resetClick() {
+    this.currentTime = 0;
     // ... your code goes here
   }
   splitClick() {
+    let minInTwoDigit = this.twoDigitsNumber(this.getMinutes());
+    let secInTwoDigit = this.twoDigitsNumber(this.getSeconds());
+
+    return minInTwoDigit + ":"+ secInTwoDigit;
     // ... your code goes here
   }
 }
